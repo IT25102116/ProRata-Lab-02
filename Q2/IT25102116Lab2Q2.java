171666584d955149a5d2533196b0264f88a157ce
IT25102116 <it25102116@my.sliit.lk>
2026-09-20 21:09:36 +0530

    Add files via upload

---
+++ Q2/IT25102116Lab2Q2.java
@@ -0,0 +1,16 @@
+public class IT25102116Lab2Q2 {
+
+	public static void main(String[] args) {
+	
+		double PI,P,L,r;
+		
+		PI = 3.14;
+		L = 10;
+		P = 4*L;
+		r = P/(2*PI);
+		
+		System.out.println("Radius r :" + r);
+	
+	}
+
+}

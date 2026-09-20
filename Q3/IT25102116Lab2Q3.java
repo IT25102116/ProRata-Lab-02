2dc1a2abf5741d3e057f88714596d7928074db18
IT25102116 <it25102116@my.sliit.lk>
2026-09-20 21:10:15 +0530

    Add files via upload

---
+++ Q3/IT25102116Lab2Q3.java
@@ -0,0 +1,17 @@
+public class IT25102116Lab2Q3 {
+
+	public static void main(String[] args) {
+	
+		double H;
+		int A,B;
+		
+		A = 3;
+		B = 4;
+		
+		H = Math.sqrt((A*A)+(B*B));
+		
+		System.out.println("Hypotenuse H:" + H);
+	
+	}
+
+}

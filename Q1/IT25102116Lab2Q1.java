7e3b8c364bbb50a5ab69f7cc513e37a831fb9978
IT25102116 <it25102116@my.sliit.lk>
2026-09-20 21:09:19 +0530

    Add files via upload

---
+++ Q1/IT25102116Lab2Q1.java
@@ -0,0 +1,17 @@
+public class IT25102116Lab2Q1 {
+
+	public static void main(String[] args) {
+	
+		double L,W,P;
+		
+		P = 100;
+		
+		L = 2*P/7;
+		W = L*3/4;
+		
+		System.out.println("Length L : " + L);
+		System.out.println("Width W : " + W);
+	
+	}
+
+}
